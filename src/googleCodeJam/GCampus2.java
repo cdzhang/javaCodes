51f287c58d712cf841b3c5d6abeeb10adc1641dc
cdzhang <cdzhang01@foxmail.com>
2016-02-26 16:13:45 +0800

    no idea

---
+++ src/googleCodeJam/GCampus2.java
@@ -0,0 +1,25 @@
+package googleCodeJam;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class GCampus2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+	public void solve() throws IOException{
+		String inputFile = Tools.chooseFile();
+		String outputFile = Tools.getOutputName(inputFile);
+		BufferedReader in = new BufferedReader(new FileReader(inputFile));
+		PrintWriter out = new PrintWriter(outputFile);
+		int T = Integer.parseInt(in.readLine());//first line
+		for(int caseN=1; caseN<=T;caseN++){
+			
+		}
+		in.close();
+		out.close();
+	}
+ }
